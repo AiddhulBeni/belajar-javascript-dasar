e5fd043aedb6dd720028c6b89fb7b59664b64254
AiddhulBeni <aidulbeni@gmail.com>
2023-06-20 10:20:16 +0700

    update belajar javascript dasar

---
+++ perulangan/for-loop/script.js
@@ -0,0 +1,10 @@
+let counter = 1;
+
+for(;counter <= 100;){
+    document.writeln(`<p>${counter}. Beni</p>`);
+    counter++
+};
+
+for(let counter = 1;counter <= 10;counter++){
+    document.writeln(`<p>${counter}. Beni</p>`);
+};
