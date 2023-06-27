ef1dd703ebeaf63b28b580165ca9a072668d9467
AiddhulBeni <aidulbeni@gmail.com>
2023-06-27 15:39:49 +0700

    update belajar javascript dasar

--- perulangan/for-loop/for.js
+++ perulangan/for-loop/for.js
@@ -17,11 +17,19 @@ for (let counter = 1; counter <= 2; counter++) {
     document.writeln(`<p>${counter}. Beni</p>`);
 };*/
 
-// for (let i = 1; i <= 5; i++) {
-//   document.writeln(`<p>Perulangan ke-${i}</p>`);
-// }
+for (let i = 0; i <= 5; i++) {
+   document.writeln(`<p>Perulangan ke-${i + 1}</p>`);
+}
 
-const clasmateNames = ["Abdul", "Beni", "Putra", "Jaran", "Kembang"];
+const clasmateNames = ["Abdul", "Beni", "Putra", "Jaran", "Kembang", "Bendot"];
 for (let a = 0; a < clasmateNames.length; a++) {
   document.writeln(`<p>${clasmateNames[a]}</p>`);
 }
+
+
+let x = 0;
+for(let i=0;i<4;i++) {
+  console.log(i)
+ x+=i; //x = x + i + i + i + i (x = x +0+1+2+3)
+}
+console.log(x);
---
+++ switch-statement/switch.js
@@ -0,0 +1,19 @@
+let data = 70;
+
+switch (data) {
+  case 90:
+    document.writeln(`<p>Nilai kamu adalah : ${data}</p>`);
+    break;
+  case 80:
+    document.writeln(`<p>Nilai kamu adalah : ${data}</p>`);
+    break;
+  default:
+    document.writeln(`<p>Nilai kamu adalah : ${data}</p>`);
+}
+
+//for loop
+const pet = ["dog", "cat", "horse", "chicken", "snack"];
+
+for (let i = 0; i < pet.length; i++) { 
+        document.writeln(`<p>${pet[i]}</p>`)
+} 
