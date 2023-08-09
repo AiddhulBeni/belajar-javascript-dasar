d66f8dbc42d9a86d953ed64ec03fe8773e277aa4
Aiddhul Beni Putra <aidulbeni@gmail.com>
2023-08-09 22:46:05 +0700

    update belajar javascript dasar

--- latihan/slot-games/slot-games.js
+++ latihan/slot-games/slot-games.js
@@ -1,34 +1,23 @@
 const deposit = () => {
-  const depositAmount = Number(document.getElementById("deposit").value);
   const balance = document.getElementById("balance");
   const submitButton = document.getElementById("submitButton");
 
   submitButton.addEventListener("click", () => {
+    const depositAmount = Number(document.getElementById("deposit").value);
+
     if (isNaN(depositAmount) || depositAmount < 0) {
-      balance.innerText = "Please enter a positive number";
+      balance.innerText = "Please enter a valid number";
     } else {
       balance.innerText = depositAmount;
     }
+    return depositAmount;
   });
+
 };
 
 deposit();
-
 console.log(deposit());
 
-// const depositButton = document.getElementById("submitButton");
-// const saldo = document.getElementById("balance");
-
-// depositButton.addEventListener("click", () => {
-//   const depositAmount = Number(document.getElementById("deposit").value);
-
-//   if (isNaN(depositAmount) || depositAmount < 0) {
-//     saldo.innerText = "Please enter a positive number";
-//   } else {
-//     saldo.innerText = depositAmount;
-//   }
-
-//   console.log(depositAmount); 
-// });
-
-
+const numberOfLines = () => {
+  const numberOfLines = document.getElementById()
+}
